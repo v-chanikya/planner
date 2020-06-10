b217f33792334fbe58cf7712c23ae42e257d698f
V Chanikya Reddy <vchanikya.reddy@nxp.com>
2020-06-10 23:31:28 +0530

    optimized the tasks and added parent tracker
    Signed-off-by: V Chanikya Reddy <vchanikya.reddy@nxp.com>

--- webapp/src/Tasks.js
+++ webapp/src/Tasks.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import {IonGrid, IonRow, IonCol, IonSplitPane} from '@ionic/react'
-import {IonList, IonItem, IonLabel, IonInput, IonDatetime, IonText, IonItemDivider, IonButton, IonTextarea} from '@ionic/react'
+import {IonRow, IonCol} from '@ionic/react'
+import {IonList, IonItem, IonLabel, IonInput, IonDatetime, IonText, IonButton, IonTextarea} from '@ionic/react'
 import {IonCard, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCardContent, IonIcon} from '@ionic/react'
 import {add, chevronForward, chevronBack, caretForward} from 'ionicons/icons'
 /* Ionic css styles */
@@ -71,8 +71,11 @@ class AddTask extends React.Component{
 
 class Task extends React.Component{
     render(){
+        var color = {background:"white"};
+        if(this.props.selected === this.props.task_data.task_id)
+            color = {background:"lightgrey"};
         return (
-            <IonCard>
+            <IonCard style={color}>
                 <IonRow>
                     <IonCol>
                         <IonCardHeader>
@@ -105,32 +108,15 @@ class Task extends React.Component{
     }
 }
 
-class TasksList extends React.Component{
-        render(){
-        const tasks = this.props.tasks;
-        return (
-                    <div style={{width:"100%"}}>
-                        {tasks.map(task=>(
-                            <Task
-                                task_data={task}
-                                subtasks={this.props.subtasks}
-                                supertasks={this.props.supertasks}
-                                newtaskpane={this.props.newtaskpane}/>
-                        ))}
-                    </div>
-            )
-    }
-}
-
 class Tasks extends React.Component{
     constructor(props){
         super(props);
         this.state = {
             panestack: [
-                {"tasks":[]},
-                {"tasks":[]},
-                {"tasks":[]},
-                {"tasks":[]}
+                {"tasks":[], "selected":0},
+                {"tasks":[], "selected":0},
+                {"tasks":[], "selected":0},
+                {"tasks":[], "selected":0}
             ]
         }
         this.action_state = "task"
@@ -147,19 +133,20 @@ class Tasks extends React.Component{
         .then(data => {
             var i = 0;
             if (data.length > 0){
-                var task_pane = [{"tasks":data}];
+                var task_pane = [{"tasks":data, "selected":0}];
                 var task_panes = this.state.panestack.slice();
+                task_panes[pane_no].selected = task_id;
                 if(req_type === "child"){
                     task_panes = task_pane.concat(task_panes.slice(pane_no,4));
                     for(i=0; pane_no > 0 && i < (3-(4-pane_no)); i++){
-                        task_panes.push({"tasks":[]});
+                        task_panes.push({"tasks":[], "selected":0});
                     }
                     this.setState({"panestack":task_panes});
                 } else if(req_type === "sibling"){
                     task_panes.shift();
                     task_panes.concat(task_pane);
                     for(i=0; i < 4 - task_panes.length; i++){
-                        task_panes.push({"tasks":[]});
+                        task_panes.push({"tasks":[], "selected":0});
                     }
                     this.setState({"panestack":task_panes});
                 }
@@ -168,8 +155,9 @@ class Tasks extends React.Component{
         });
     }
     getchildtasks(task_id, pane_no){
-        if(this.action_state === "task")
+        if(this.action_state === "task"){
             this.gettasks("/api/getChildren", task_id, pane_no, "child");
+        }
     }
     getsiblingtasks(task_id, pane_no){
         if(this.action_state === "task")
@@ -198,39 +186,22 @@ class Tasks extends React.Component{
     render(){
         return (
             <div>
-                <div id="pane3">
-                    <TasksList
-                        tasks={this.state.panestack[3].tasks}
-                        subtasks={task_id=>this.getchildtasks(task_id,3)}
-                        supertasks={task_id=>this.getsiblingtasks(task_id,3)}
-                        newtaskpane={(task_id,add)=>this.newtaskpane(task_id,add,3)}/>
-                </div>
-                <div id="pane2">
-                    <TasksList
-                        tasks={this.state.panestack[2].tasks}
-                        subtasks={task_id=>this.getchildtasks(task_id,2)}
-                        supertasks={task_id=>this.getsiblingtasks(task_id,2)}
-                        newtaskpane={(task_id,add)=>this.newtaskpane(task_id,add,2)}/>
-                </div>
-                <div id="pane1">
-                    <TasksList
-                        tasks={this.state.panestack[1].tasks}
-                        subtasks={task_id=>this.getchildtasks(task_id,1)}
-                        supertasks={task_id=>this.getsiblingtasks(task_id,1)}
-                        newtaskpane={(task_id,add)=>this.newtaskpane(task_id,add,1)}/>
-                </div>
-                <div id="pane0">
-                    {this.state.panestack[0].pane_type === "add"
-                    ?<AddTask
-                        parentid={this.state.panestack[0].task_id}
-                        newtaskpane={(task_id,add)=>this.newtaskpane(task_id,add,this.state.panestack[0].pane_no)}/>
-                    :<TasksList
-                        tasks={this.state.panestack[0].tasks}
-                        subtasks={task_id=>this.getchildtasks(task_id,0)}
-                        supertasks={task_id=>this.getsiblingtasks(task_id,0)}
-                        newtaskpane={(task_id,add)=>this.newtaskpane(task_id,add,0)}/>
-                    }
-                </div>
+                {this.state.panestack.map((tasks,index)=>(
+                    <div id={"pane".concat(index)}>
+                        {(index === 0 && tasks.pane_type === "add")
+                            ?<AddTask
+                                parentid={tasks.task_id}
+                                newtaskpane={(task_id,add)=>this.newtaskpane(task_id,add,tasks.pane_no)}/>
+                            :tasks.tasks.map((task)=>(
+                                <Task
+                                    task_data={task}
+                                    selected={tasks.selected}
+                                    subtasks={task_id=>this.getchildtasks(task_id,index)}
+                                    supertasks={task_id=>this.getsiblingtasks(task_id,index)}
+                                    newtaskpane={(task_id,add)=>this.newtaskpane(task_id,add,index)}/>))
+                        }
+                    </div>
+                ))}
             </div>
         )
     }
